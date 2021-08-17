888f281b15ddb8020fce77e81f639f3b52515b6a
David Owusu-Antwi <david.owusuantwi@gmail.com>
2021-08-17 13:13:45 -0500

    added visualization label and parameter map updates

--- update/visualize.py
+++ update/visualize.py
@@ -101,9 +101,12 @@ class Visualizer(QtWidgets.QWidget):
         super().__init__()
         self.resize(1500, 900)
         self.simulator = simulator
+        self.trial_label = None
+        self.noise_level_label = None
+        self.circuit_size_label = None
         layout = self.build()
         self.layout = layout
-        self.circuit_image = None
+        self.circuit_image_canvas = None
         self.parameter_map = None
         timer = self.setup_timer()
         self.timer = timer
@@ -145,15 +148,28 @@ class Visualizer(QtWidgets.QWidget):
         toggle.clicked.connect(self.toggle)
         layout.addWidget(toggle, *(1, 0, 1, 2))
 
-        trial_label = QtWidgets.QLabel("Trial Number:")
-        noise_level_label = QtWidgets.QLabel("Noise Level:")
-        circuit_size_label = QtWidgets.QLabel("Circuit Size:")
+        trial_label_title = QtWidgets.QLabel("Trial Number:")
+        noise_level_label_title = QtWidgets.QLabel("Noise Level:")
+        circuit_size_label_title = QtWidgets.QLabel("Circuit Size:")
+        trial_label_title.setAlignment(QtCore.Qt.AlignCenter)
+        noise_level_label_title.setAlignment(QtCore.Qt.AlignCenter)
+        circuit_size_label_title.setAlignment(QtCore.Qt.AlignCenter)
+        trial_label = QtWidgets.QLabel("")
+        noise_level_label = QtWidgets.QLabel("")
+        circuit_size_label = QtWidgets.QLabel("")
         trial_label.setAlignment(QtCore.Qt.AlignCenter)
         noise_level_label.setAlignment(QtCore.Qt.AlignCenter)
         circuit_size_label.setAlignment(QtCore.Qt.AlignCenter)
-        layout.addWidget(trial_label, *(2, 0, 1, 1))
-        layout.addWidget(noise_level_label, *(3, 0, 1, 1))
-        layout.addWidget(circuit_size_label, *(4, 0, 1, 1))
+        layout.addWidget(trial_label_title, *(2, 0, 1, 1))
+        layout.addWidget(trial_label, *(2, 1, 1, 1))
+        layout.addWidget(noise_level_label_title, *(3, 0, 1, 1))
+        layout.addWidget(noise_level_label, *(3, 1, 1, 1))
+        layout.addWidget(circuit_size_label_title, *(4, 0, 1, 1))
+        layout.addWidget(circuit_size_label, *(4, 1, 1, 1))
+
+        self.trial_label = trial_label
+        self.noise_level_label = noise_level_label
+        self.circuit_size_label = circuit_size_label
 
         return layout
 
@@ -167,7 +183,7 @@ class Visualizer(QtWidgets.QWidget):
         layout.addWidget(circuit_image_canvas, *(1, 2, 4, 4))
         parameter_map_canvas = Canvas(parameter_map)
         layout.addWidget(parameter_map_canvas, *(5, 0, 4, 6))
-        self.circuit_image = circuit_image
+        self.circuit_image_canvas = circuit_image_canvas
         self.parameter_map = parameter_map
 
 
@@ -175,6 +191,50 @@ class Visualizer(QtWidgets.QWidget):
         """
         Toggles (i.e., pauses or resumes) simulation run.
         """
+
+
+    def update_labels(self, trial, noise_level, circuit_size):
+        """
+        Updates GUI labels.
+        """
+        trial_label = self.trial_label
+        noise_level_label = self.noise_level_label
+        circuit_size_label = self.circuit_size_label
+        trial_label.setText(str(trial))
+        noise_level_label.setText(str(noise_level))
+        circuit_size_label.setText(str(circuit_size))
+
+
+    def update_figures(self, gate_parameters, random_circuit):
+        """
+        Updates circuit image and plot of gate parameters.
+        """
+        circuit_image_canvas = self.circuit_image_canvas
+        parameter_map = self.parameter_map
+        layout = self.layout
+        circuit = random_circuit.circuit
+
+        plt.figure(parameter_map.number)
+        beta_parameter, gamma_parameter = gate_parameters
+        plt.scatter([beta_parameter], [gamma_parameter],
+                    marker='x', color='r', s=250, linewidths=5)
+        parameter_map.canvas.draw()
+
+        #new_circuit_image = circuit.draw(output="mpl")
+        #new_circuit_image_canvas = Canvas(new_circuit_image)
+        #layout.removeWidget(circuit_image_canvas)
+        #circuit_image_canvas.close()
+        #layout.addWidget(new_circuit_image_canvas, *(1, 2, 4, 4))
+        #layout.update()
+        #self.circuit_image_canvas = new_circuit_image_canvas
+
+        #new_circuit_image = circuit.draw(output="mpl")
+        #new_circuit_image_canvas = Canvas(new_circuit_image)
+        #layout.replaceWidget(circuit_image_canvas, new_circuit_image_canvas)
+        #self.circuit_image_canvas = new_circuit_image_canvas
+
+        #plt.figure(circuit_image.number)
+        #circuit.draw(output="mpl")
 
 
     def update(self):
@@ -189,12 +249,12 @@ class Visualizer(QtWidgets.QWidget):
         else:
             #TODO: add circuit and plot updating...
             trial, noise_level, circuit_size = next_step
-            #self.update_labels(trial, noise_level, circuit_size)
+            self.update_labels(trial, noise_level, circuit_size)
             (gate_parameters,
              random_circuit) = simulator.build_trial(trial,
                                                      noise_level,
                                                      circuit_size)
-            #self.update_figures()
+            self.update_figures(gate_parameters, random_circuit)
             simulator.run(trial, noise_level, circuit_size, random_circuit)
 
 
@@ -206,12 +266,16 @@ class Visualizer(QtWidgets.QWidget):
         self.application_manager.exec_()
 
 
-def build_parameter_map():
+def build_parameter_map(max_gate_parameters):
     """
     Gets sample parameter map.
     """
+    max_beta_parameter, max_gamma_parameter = max_gate_parameters
     figure = plt.figure()
-    plt.scatter([5], [5], marker='x', color='r', s=250, linewidths=5)
+    plt.scatter([0], [0], marker='x', color='k', s=250, linewidths=5)
+    plt.title("parameter map")
+    plt.xlim(0, max_beta_parameter)
+    plt.ylim(0, max_gamma_parameter)
     figure = plt.gcf()
     return figure
 
@@ -237,7 +301,8 @@ def main():
     simulator = SimulatorInterface(NOISE_LEVEL_OPTION)
     visualizer = Visualizer(simulator)
     circuit_image = build_circuit_image()
-    parameter_map = build_parameter_map()
+    max_gate_parameters = simulator.max_gate_parameters
+    parameter_map = build_parameter_map(max_gate_parameters)
     visualizer.embed_matplotlib_objects(circuit_image, parameter_map)
     visualizer.run()
     return visualizer
